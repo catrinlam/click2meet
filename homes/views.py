bc7546f5a2a3fe79b29d07935f5571e3df3bce8f
leo-gwe <lionel.gweshe@yahoo.com>
2025-02-17 12:43:27 +0000

    Add links to events and bookings pages

--- homes/views.py
+++ homes/views.py
@@ -7,3 +7,9 @@ def home(request):
         'events': events
     }
     return render(request, 'homes/index.html', context)
+
+def features(request):
+    return render(request, 'features.html')
+
+def bookings(request):
+    return render(request, 'bookings.html')
