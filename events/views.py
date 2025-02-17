b7afc2e14299ae7e688bf8aedbaba9b1e375a4f9
Catrin <catrin.lam@gmail.com>
2025-02-17 16:21:38 +0000

    Add city and date filters to event list view

--- events/views.py
+++ events/views.py
@@ -6,6 +6,9 @@ from django.db.models import Q
 
 def EventListView(request):
     search_query = request.GET.get('search', '')
+    city_filter = request.GET.get('city', '')
+    date_filter = request.GET.get('date', '')
+
     events = Event.objects.all().prefetch_related('images').order_by('-created_at')
 
     if search_query:
@@ -16,9 +19,17 @@ def EventListView(request):
             Q(city__icontains=search_query)
         )
 
+    if city_filter:
+        events = events.filter(city__icontains=city_filter)
+
+    if date_filter:
+        events = events.filter(date=date_filter)
+
     context = {
         'events': events,
         'search_query': search_query,
+        'city_filter': city_filter,
+        'date_filter': date_filter,
     }
     return render(request, 'events/event_list.html', context)
 
